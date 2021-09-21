4614a214a50af2f27412f145d3776762b8ee5c4d
Kelly (KT) Thompson <kgt@lanl.gov>
2021-09-21 15:47:33 -0600

    Add some output examples from Caliper.

--- src/diagnostics/Timing.hh
+++ src/diagnostics/Timing.hh
@@ -158,6 +158,95 @@ public:
  *      have_caliper -> dratimer [label="no"];
  * }
  * \enddot
+ *
+ * If Caliper timers are used, simple reports can be generated like this:
+ *
+ * \verbatim
+ $ CALI_CONFIG_PROFILE=runtime-report ./tstTiming
+
+  Path            Inclusive time Exclusive time Time %
+  Outer                 0.000710       0.000014  1.680672
+    C_iteration         0.000116       0.000116 13.925570
+    B_iteration         0.000231       0.000231 27.731092
+    A_iteration         0.000349       0.000349 41.896759
+ * \endverbatim
+ *
+ * Or with MPI like this:
+ *
+ * \verbatim
+$ CALI_CONFIG_PROFILE=runtime-report mpiexec -n 2 ../bin/mcgrid -i amr_xyz_mg/mginf01
+...
+Path                                 Inclusive time Exclusive time Time %
+execute_IMC:regroup                        0.006950       0.006950  9.675084
+execute_IMC:run_IMC                        0.012787       0.003883  5.405518
+  host_loop                                0.008904       0.000609  0.847788
+    trasport_time                          0.008295       0.008295 11.547457
+execute_IMC:cycle_init                     0.024644       0.005712  7.951666
+  Build_Source                             0.011629       0.008668 12.066709
+    build_Source:comb_census               0.000798       0.000027  0.037587
+      rep_comb_census                      0.000771       0.000771  1.073308
+    build_Source:calc_src_num              0.000443       0.000443  0.616700
+    build_Source:build_strategy            0.000593       0.000474  0.659855
+      calc_evol                            0.000119       0.000119  0.165660
+    build_Source:init_cen_nrg              0.001127       0.001087  1.513211
+      calc_evol                            0.000040       0.000040  0.055684
+  Build_Mat_State                          0.004806       0.004806  6.690425
+  cycle_init:fine_tune_census_cell         0.002497       0.002497  3.476070
+ * \endverbatim
+ *
+ * You can also generate MPI timing reports like this:
+ *
+ * \verbatim
+ $ CALI_CONFIG_PROFILE=mpi-runtime-report mpiexec -n 2 ../bin/mcgrid -i amr_xyz_mg/mginf01
+...
+Path                                 Min time/rank Max time/rank Avg time/rank Time % (total)
+MPI_Finalize                              0.000007      0.000042      0.000024       0.036926
+MPI_Comm_free                             0.000048      0.000054      0.000051       0.076866
+MPI_Type_size                             0.000006      0.000007      0.000006       0.009797
+MPI_Type_commit                           0.000008      0.000009      0.000008       0.012811
+MPI_Type_create_struct                    0.000022      0.000023      0.000022       0.033911
+MPI_Scan                                  0.000013      0.000022      0.000018       0.026375
+MPI_Allreduce                             0.000083      0.016863      0.008473      12.770254
+MPI_Barrier                               0.000091      0.006369      0.003230       4.868160
+MPI_Type_free                             0.000017      0.000018      0.000017       0.026375
+execute_IMC:regroup                       0.001608      0.001620      0.001614       2.432573
+  MPI_Allreduce                           0.000395      0.000408      0.000402       0.605129
+execute_IMC:run_IMC                       0.003570      0.003671      0.003621       5.456710
+  host_loop                               0.000556      0.000559      0.000557       0.840247
+    trasport_time                         0.007417      0.007493      0.007455      11.235955
+execute_IMC:cycle_init                    0.005869      0.005936      0.005903       8.896073
+  Build_Source                            0.002678      0.002696      0.002687       4.049767
+    MPI_Send                              0.000029      0.000029      0.000029       0.021854
+    build_Source:comb_census              0.000020      0.000020      0.000020       0.030143
+      MPI_Allreduce                       0.000010      0.000012      0.000011       0.016579
+      rep_comb_census                     0.000618      0.000657      0.000638       0.960821
+        MPI_Allreduce                     0.000028      0.000066      0.000047       0.070837
+    build_Source:calc_src_num             0.000384      0.000402      0.000393       0.592318
+      MPI_Send                            0.000020      0.000020      0.000020       0.015072
+      MPI_Barrier                         0.000023      0.000065      0.000044       0.066315
+      MPI_Get_count                       0.000013      0.000013      0.000013       0.009797
+      MPI_Recv                            0.000030      0.000030      0.000030       0.022608
+    build_Source:build_strategy           0.000406      0.000420      0.000413       0.622461
+      MPI_Send                            0.000012      0.000012      0.000012       0.009043
+      MPI_Barrier                         0.000015      0.000043      0.000029       0.043708
+      MPI_Get_count                       0.000010      0.000010      0.000010       0.007536
+      MPI_Recv                            0.000025      0.000025      0.000025       0.018840
+      MPI_Allreduce                       0.000039      0.000050      0.000044       0.067069
+      calc_evol                           0.000093      0.000096      0.000094       0.142428
+    MPI_Allreduce                         0.000302      0.000306      0.000304       0.458180
+    build_Source:init_cen_nrg             0.000544      0.000553      0.000549       0.826683
+      MPI_Send                            0.000010      0.000010      0.000010       0.007536
+      MPI_Barrier                         0.000012      0.000033      0.000022       0.033911
+      MPI_Get_count                       0.000007      0.000007      0.000007       0.005275
+      MPI_Recv                            0.000021      0.000021      0.000021       0.015825
+      calc_evol                           0.000040      0.000042      0.000041       0.061794
+    MPI_Barrier                           0.000022      0.000093      0.000058       0.086662
+    MPI_Get_count                         0.000010      0.000010      0.000010       0.007536
+    MPI_Recv                              0.005526      0.005526      0.005526       4.164312
+  Build_Mat_State                         0.004419      0.004634      0.004526       6.822207
+  cycle_init:fine_tune_census_cell        0.001510      0.001541      0.001525       2.299188
+    MPI_Allreduce                         0.000039      0.000100      0.000069       0.104748
+ * \endverbatim
  */
 
 //------------------------------------------------------------------------------------------------//
